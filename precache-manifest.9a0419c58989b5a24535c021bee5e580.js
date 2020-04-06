6e260058ee781d813b378fa1ea3454998b8bfa67
Zakiulhassan <zakihassan555@gmail.com>
2020-04-06 20:16:54 +0500

    Updates

---
+++ precache-manifest.9a0419c58989b5a24535c021bee5e580.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39f140bf39f147b399f28e0339a1b7df",
+    "url": "/educate-online-react-app/index.html"
+  },
+  {
+    "revision": "fec86b4da0697ded2c68",
+    "url": "/educate-online-react-app/static/css/main.b1ae24db.chunk.css"
+  },
+  {
+    "revision": "0df5ac49d1e3d5baa018",
+    "url": "/educate-online-react-app/static/js/2.999201d4.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/educate-online-react-app/static/js/2.999201d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fec86b4da0697ded2c68",
+    "url": "/educate-online-react-app/static/js/main.7241a84d.chunk.js"
+  },
+  {
+    "revision": "3b0b426f5ddcf2227669",
+    "url": "/educate-online-react-app/static/js/runtime-main.f1eab3f1.js"
+  },
+  {
+    "revision": "8adfb47ce20b5abab4fadb77a935c9db",
+    "url": "/educate-online-react-app/static/media/angular.8adfb47c.jpg"
+  },
+  {
+    "revision": "da9d6362280aab6a121aa0b34a7b9ad8",
+    "url": "/educate-online-react-app/static/media/banner-1.da9d6362.png"
+  },
+  {
+    "revision": "6b7ec32ebfabd4fb73acddb699fa5a82",
+    "url": "/educate-online-react-app/static/media/full-stack.6b7ec32e.jpg"
+  },
+  {
+    "revision": "f987446325d14eebeea8581f8b0e2aa9",
+    "url": "/educate-online-react-app/static/media/learn.f9874463.jpg"
+  },
+  {
+    "revision": "e451017bcb8a52e71497ba7019a1fb98",
+    "url": "/educate-online-react-app/static/media/node.e451017b.jpg"
+  },
+  {
+    "revision": "df4e0227c07ef1fd24068ec587601a97",
+    "url": "/educate-online-react-app/static/media/person-1.df4e0227.jpg"
+  },
+  {
+    "revision": "9e1fc6898f9d7c9a2352128950d0bd2b",
+    "url": "/educate-online-react-app/static/media/seo.9e1fc689.jpg"
+  },
+  {
+    "revision": "828f0d7f848d88647b0afcc032c4573d",
+    "url": "/educate-online-react-app/static/media/team.828f0d7f.jpg"
+  }
+]);
